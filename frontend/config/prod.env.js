e4ec760c2453d50f3251064efeb66e4a0806ce77
Xavier Bustamante Talavera <xavier@bustawin.com>
2017-11-06 17:02:18 +0100

    General cleanup; move frontend to own project; reformat upload_jsons

--- frontend/config/prod.env.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  NODE_ENV: '"production"',
-  SERVER_URL: '"http://192.168.2.2:8090"'
-}
---
+++ setup.py
@@ -0,0 +1,37 @@
+from setuptools import find_packages, setup
+
+tests_require = [
+    'assertpy'
+]
+
+setup(
+    name='eReuse-WorkbenchServer',
+    version='0.2',
+    packages=find_packages(exclude=('contrib', 'docs', 'scripts')),
+    url='https://github.com/eReuse/acelerywb',
+    license='AGPLv3 License',
+    author='Garito and eReuse.org team',
+    author_email='x.bustamante@ereuse.org',
+    description='Workbench manager for servers',
+    # Updated in 2017-07-29
+    install_requires=[],
+    keywords='eReuse.org Workbench devices reuse recycle it asset management',
+    # http://setuptools.readthedocs.io/en/latest/setuptools.html#declaring-extras-optional-features-with-their-own-dependencies
+    # Install it with pip install .[docs] or pip install -e .[docs]
+    test_suite='ereuse_devicehub.tests',
+    tests_require=tests_require,
+    include_package_data=True,
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Framework :: Flask',
+        'Intended Audience :: Manufacturing',
+        'License :: OSI Approved :: GNU Affero General Public License v3',
+        'Natural Language :: English',
+        'Operating System :: Linux',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Office/Business',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content'
+    ]
+)
